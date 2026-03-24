3fa29787c34385cbbe2b0cbb624940449b417520
Markus Ekerheim <markus.ekerheim@gmail.com>
2026-03-24 16:59:15 +0100

    feat: Initialize a new React project using Vite, Tailwind CSS, and configure GitHub Pages deployment.

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+  plugins: [react()],
+  // Sets the base path for GitHub Pages deployment
+  base: '/ekerheim_interiors_web/',
+  build: {
+    outDir: 'dist'
+  }
 })
